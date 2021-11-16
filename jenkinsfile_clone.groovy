17c6542aafa9ff7ecbbb08a6c09deb4043b306cf
99yaswanth <yashwanth24k@gmail.com>
2021-11-16 13:03:15 +0530

    updated mvn

--- jenkinsfile_clone.groovy
+++ jenkinsfile_clone.groovy
@@ -5,11 +5,14 @@ pipeline{
         stage("checkout"){
             steps{
                 println "cloning"
+                sh "ls -l"
             }
         }
         stage("build"){
             steps{
                 println "mvn package"
+                sh "mvn clean package"
+                sh "ls -l target/"
             }
         }
     }
